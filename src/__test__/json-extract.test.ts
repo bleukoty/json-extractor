bacde41756ff4084d69452851ec813ddf913c867
kotyB <bleukoty@gmail.com>
2020-11-20 18:05:26 +0000

    test

--- src/__test__/json-extract.test.ts
+++ src/__test__/json-extract.test.ts
@@ -1,8 +1,10 @@
 import { jsonExtract } from '../index';
 test('check all complexity', () => {
-    const myData = { hello: "salut", test: "lolo", nom: { id: "10", person: {firstname: 'nom_1',lastname: 'nom_2'} }};
-    const propertyToExtract = ["hello", "test", "nom/id@user_id", "nom/person/firstname+lastname@fullname"];
-    const value = jsonExtract(myData, propertyToExtract);
+  const myData = { hello: 'salut', test: 'lolo', nom: { id: '10', person: { firstname: 'nom_1', lastname: 'nom_2' } } };
+  const propertyToExtract = ['hello', 'test', 'nom/id@user_id', 'nom/person/firstname+lastname@fullname'];
+  const value = jsonExtract(myData, propertyToExtract);
 
-    expect('salut').toBe('salut');
-})
+  expect(value.hello).toBe('salut');
+  expect(value.user_id).toBe('10');
+  expect(value.fullname).toBe('nom_1 nom_2');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,39 @@
 /**
- * 
+ *
  * @param dataJson : data in json format
  * @param keys : Array of property string to read
  */
-export const jsonExtract = (dataJson: any, keys: Array<string>): any => {
- 
-    let custom_key = "";
-    let deepJson: any = null ;
-    let custom_key_renaming = "";
-    let properties:any = [];
-  
-    return keys.reduce((acc, key) => {
-      [properties, custom_key_renaming] = key.split('@');
-      properties.split("/").forEach((item: any, index: number) => {
-        if(item.indexOf('+') !== -1){
-          custom_key += item.replace('+', '__');
-          deepJson = item.split('+').reduce((acc_2: any, value: string) => {
-              acc_2 += `${(deepJson || dataJson)[value]} `;
-              return acc_2;
-          }, '').trim();
-        } else {
-          custom_key += `${item}__`;
-          deepJson = index === 0 ? dataJson[item] : deepJson[item];
-        }  
-      });
-  
-      custom_key = custom_key.replace(/__$/, "");
-      acc = { ...acc, [custom_key_renaming ?? custom_key]: deepJson };
-  
-      // clean variable
-      custom_key = "";
-      deepJson = null;
-  
-      return acc;
-    }, {});
-  };
+export const jsonExtract = (dataJson: any, keys: any): any => {
+  let customKey = '';
+  let deepJson: any = null;
+  let customKeyRenaming = '';
+  let properties: any = [];
+
+  return keys.reduce((acc: any, key: any) => {
+    [properties, customKeyRenaming] = key.split('@');
+    properties.split('/').forEach((item: any, index: number) => {
+      if (item.indexOf('+') !== -1) {
+        customKey += item.replace('+', '__');
+        deepJson = item
+          .split('+')
+          .reduce((acc2: any, value: string) => {
+            acc2 += `${(deepJson || dataJson)[value]} `;
+            return acc2;
+          }, '')
+          .trim();
+      } else {
+        customKey += `${item}__`;
+        deepJson = index === 0 ? dataJson[item] : deepJson[item];
+      }
+    });
+
+    customKey = customKey.replace(/__$/, '');
+    acc = { ...acc, [customKeyRenaming ?? customKey]: deepJson };
+
+    // clean variable
+    customKey = '';
+    deepJson = null;
+
+    return acc;
+  }, {});
+};
