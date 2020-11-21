91553bbe38f512a6d116fb511c74f2017260cc6e
BLEU Koty <bleukoty@gmail.com>
2020-11-21 01:02:25 +0000

    ok

--- src/__test__/json-extract.test.ts
+++ src/__test__/json-extract.test.ts
@@ -1,9 +1,12 @@
 import { jsonExtract } from '../index';
 test('check all complexity', () => {
-  const myData = { greeting: 'salut', test: 'lolo', nom: { id: '10', person: { firstname: 'nom_1', lastname: 'nom_2' } } };
+  const myData = {
+    greeting: 'salut',
+    test: 'lolo',
+    nom: { id: '10', person: { firstname: 'nom_1', lastname: 'nom_2' } },
+  };
   const propertyToExtract = ['greeting', 'test', 'nom/id@user_id', 'nom/person/firstname+lastname@fullname'];
   const value = jsonExtract(myData, propertyToExtract);
 
   expect(value).toEqual({ greeting: 'salut', test: 'lolo', user_id: '10', fullname: 'nom_1 nom_2' });
-
 });
